1ea4b033ea8ec34abbfaa6c081cd153f6e8a5440
NIRMAL KUMAR <nirmal.kumar.17.1@mountblue.tech>
2021-10-10 23:03:35 +0530

    create rest api method to edit post

--- src/main/java/com/project/blog/controller/PostController.java
+++ src/main/java/com/project/blog/controller/PostController.java
@@ -7,6 +7,9 @@ import com.project.blog.entity.User;
 import com.project.blog.service.CommentService;
 import com.project.blog.service.PostService;
 import com.project.blog.service.UserService;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,11 +19,13 @@ import java.security.Principal;
 import java.sql.Timestamp;
 import java.util.*;
 
-@Controller
+@RestController
 public class PostController {
     private final PostService postService;
     private final UserService userService;
     private final CommentService commentService;
+    private Object ObjectNotFoundException;
+    private Object UsernameNotFoundException;
 
     public PostController(PostService postService, UserService userService, CommentService commentService) {
         this.postService = postService;
@@ -28,52 +33,51 @@ public class PostController {
         this.commentService = commentService;
     }
 
-//    @GetMapping("/create")
-//    public Post createPost(Principal principal, Model model){
-//        Post post = new Post();
-//        Tag tag = new Tag();
-//        Optional<User> userOptional = userService.findByName(principal.getName());
-//
-//        if (userOptional.isPresent()){
-//            User user = userOptional.get();
-//            post.setAuthor(user.getName());
-//            model.addAttribute("post", post);
-//            model.addAttribute("tag", tag);
-//            return "newPost";
-//        }else {
-//            return "/fail";
-//        }
-//    }
+    @GetMapping("/create")
+    public Map<String,Object> createPost(Principal principal) throws Throwable {
+        Post post = new Post();
+        Tag tag = new Tag();
+        Optional<User> userOptional = userService.findByName(principal.getName());
+        Map<String,Object> defaultSettings = new HashMap<>();
+        if (userOptional.isPresent()){
+            User user = userOptional.get();
+            post.setAuthor(user.getName());
+            defaultSettings.put("post", post);
+            defaultSettings.put("tag", tag);
+            return defaultSettings;
+        }else {
+            throw (Throwable) UsernameNotFoundException;
+        }
+    }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public void savePost(@RequestBody Post post,
                          @RequestBody Tag tag) {
         String[] tagList = tag.getName().split(",");
         Set<Tag> tagSet = new HashSet<Tag>();
-        for(String data : tagList){
-            Tag tempTag  = new Tag();
+        for (String data : tagList) {
+            Tag tempTag = new Tag();
             tempTag.setName(data);
             tempTag.setCreatedAt(new Timestamp(System.currentTimeMillis()));
             tempTag.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
             tagSet.add(tempTag);
         }
         post.setTags(tagSet);
-        String excerpt = post.getContent().substring(0,500);
+        String excerpt = post.getContent().substring(0, 500);
         post.setExcerpt(excerpt);
-            postService.save(post);
+        postService.save(post);
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String updatePost(@ModelAttribute("post") Post post,
-                             @ModelAttribute("tag") Tag tag,
-                             BindingResult bindingResult){
+    public void updatePost(@RequestBody Post post,
+                             @RequestBody Tag tag) {
         Optional<Post> optionalPost = postService.findPostById(post.getId());
-       Post  updatePost = optionalPost.get();
+        Post updatePost = optionalPost.get();
         String[] tagList = tag.getName().split(",");
         Set<Tag> tagSet = new HashSet<Tag>();
 
-        for(String data : tagList){
-            Tag tempTag  = new Tag();
+        for (String data : tagList) {
+            Tag tempTag = new Tag();
             tempTag.setName(data);
             tempTag.setCreatedAt(new Timestamp(System.currentTimeMillis()));
             tempTag.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
@@ -85,56 +89,52 @@ public class PostController {
         updatePost.setContent(post.getContent());
         updatePost.setTitle(post.getTitle());
         updatePost.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
-        String excerpt = post.getContent().substring(0,500);
+        String excerpt = post.getContent().substring(0, 500);
         updatePost.setExcerpt(excerpt);
 
-        if (bindingResult.hasErrors()) {
-            return "fail";
-        }
-        else {
-            postService.save(updatePost);
-            return "redirect:/";
-        }
+        postService.save(updatePost);
     }
-    @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
-    public String getPost(@PathVariable Long id,
-                                Principal principal,
-                                Model model) {
+
+    @GetMapping("/post/{id}")
+    public Map<String,Object> getPost(@PathVariable Long id) {
         Optional<Post> optionalPost = postService.findPostById(id);
         List<Comment> comments = commentService.getCommentByPostId(id);
+        Post post = new Post();
 
         if (optionalPost.isPresent()) {
-            Post post = optionalPost.get();
-            model.addAttribute("post", post);
-            model.addAttribute("comments", comments);
-            if (isPrincipalOwnerOfPost(principal, post)) {
-                model.addAttribute("username", principal.getName());
-            }
-            return "/post";
-        } else {
-            return "/fail";
+            post = optionalPost.get();
         }
+//            model.addAttribute("post", post);
+//            model.addAttribute("comments", comments);
+//            if (isPrincipalOwnerOfPost(principal, post)) {
+//                model.addAttribute("username", principal.getName());
+//            }
+//
+//        }
+            Map<String,Object> postData= new HashMap<>();
+        postData.put("post", post);
+        postData.put("comments", comments);
+        return postData;
     }
 
     @RequestMapping(value = "/editPost/{id}", method = RequestMethod.GET)
-    public String editPost(@PathVariable Long id,
-                                 Principal principal,
-                                 Model model) {
+    public Map<String,Object> editPost(@PathVariable Long id,
+                                 Principal principal) throws Throwable {
         Optional<Post> optionalPost = postService.findPostById(id);
         Tag tag = new Tag();
+        Map<String,Object> objectsToEdit = new HashMap<>();
 
         if (optionalPost.isPresent()) {
             Post post = optionalPost.get();
             if (isPrincipalOwnerOfPost(principal, post)) {
-                model.addAttribute("post", post);
-                model.addAttribute("tag", tag);
-                return "/updatePost";
+                objectsToEdit.put("post", post);
+                objectsToEdit.put("tag", tag);
+                return objectsToEdit;
             } else {
-                return "/fail";
+                throw (Throwable) ObjectNotFoundException;
             }
         } else {
-            return "/fail";
-        }
+            throw (Throwable) ObjectNotFoundException;        }
     }
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.DELETE)
