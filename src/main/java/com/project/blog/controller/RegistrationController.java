9e152e32cd0eb2a2efa1592237b34d4ac729a1c9
nirmal <nirmalkumar8273@gmail.com>
2021-10-13 01:19:14 +0530

    resolve errors in rest controller

--- src/main/java/com/project/blog/controller/RegistrationController.java
+++ src/main/java/com/project/blog/controller/RegistrationController.java
@@ -7,12 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class RegistrationController {
 
     @Autowired
@@ -26,6 +23,7 @@ public class RegistrationController {
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public User registration() {
+
         return new User();
     }
 
